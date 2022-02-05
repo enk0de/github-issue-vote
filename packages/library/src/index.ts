4220a40926f25500873db5693124ccf0a2540226
임지훈 <jihoon.lim@toss.im>
2022-02-05 16:24:16 +0900

    feat(library): 테스트 목업

--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -1,2 +1,18 @@
-const a: string = "헬로";
-export default a;
+import { css } from "./stitches";
+
+const container = document.createElement("div", {});
+const containerCss = css({
+  background: "black",
+});
+container.className = containerCss();
+
+const testButton = document.createElement("button");
+const testButtonCss = css({
+  background: "white",
+  color: "blue",
+});
+testButton.className = testButtonCss();
+
+container.appendChild(testButton);
+
+export default container;
