3ef64c58ba2c0573b4927305b5c14519771d04fb
임지훈 <jihoon.lim@toss.im>
2022-02-12 17:14:51 +0900

    refactor/feat(library): turn createVote into class Vote

--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -1,42 +1,100 @@
 import createButton from './button';
+import { AGREE_REACTION_TYPE, DISAGREE_REACTION_TYPE, Reactions } from './interfaces';
 import { css } from './stitches';
 import createText from './text';
 
-interface VoteData {
-  issueId: number;
-  title: string;
+interface Config {
+  owner: string;
+  repo: string;
+  issueNumber: number;
+}
+
+interface Constructor {
+  config: Config;
+  title: string;  
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function createVote({ issueId, title }: VoteData) {
-  const containerEl = document.createElement('div');
-  const containerCss = css({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    gap: '10px',
-    padding: '10px',
-    borderRadius: '10px',
-  });
-  containerEl.className = containerCss();
+export default class Vote {
+  private containerEl: HTMLDivElement;
+  private buttonContainerEl: HTMLDivElement;
+  private titleEl: HTMLHeadingElement;
+  private agreeButtonEl: HTMLButtonElement;
+  private disagreeButtonEl: HTMLButtonElement;
 
-  const titleEl = createText({ text: title, css: { padding: '5px 0' } });
+  private config: Config;
 
-  const buttonContainerEl = document.createElement('div');
-  const buttonContainerCss = css({
-    display: 'flex',
-    gap: 10,
-  });
-  buttonContainerEl.className = buttonContainerCss();
+  constructor({ config, title }: Constructor) {
+    this.config = config;
+    this.containerEl = Vote.createContainer();
 
-  const agreeButtonEl = createButton({ text: '👍' });
-  const disagreeButtonEl = createButton({ text: '👎' });
+    this.buttonContainerEl = Vote.createButtonContainer();
+    this.titleEl = createText({ text: title, css: { padding: '5px 0' } });
+    this.agreeButtonEl = createButton({ text: '👍' });
+    this.disagreeButtonEl = createButton({ text: '👎' });
+  
+    this.buttonContainerEl.appendChild(this.agreeButtonEl);
+    this.buttonContainerEl.appendChild(this.disagreeButtonEl);
+  
+    this.containerEl.appendChild(this.titleEl);
+    this.containerEl.appendChild(this.buttonContainerEl);
 
-  buttonContainerEl.appendChild(agreeButtonEl);
-  buttonContainerEl.appendChild(disagreeButtonEl);
+    this.agreeButtonEl.addEventListener('click', async () => {
+      try {
+        const result = await this.handleReactionClick(AGREE_REACTION_TYPE);
+        console.log(result);
+      } catch(err) {
+        console.error(err)
+      }
+    })
+    this.disagreeButtonEl.addEventListener('click', async () => {
+      try {
+        const result = await this.handleReactionClick(DISAGREE_REACTION_TYPE);
+        console.log(result);
+      } catch(err) {
+        console.error(err)
+      }
+    })
+  }
 
-  containerEl.appendChild(titleEl);
-  containerEl.appendChild(buttonContainerEl);
+  private static createContainer() {
+    const containerEl = document.createElement('div');
+    const containerCss = css({
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: '10px',
+      padding: '10px',
+      borderRadius: '10px',
+    });
+    containerEl.className = containerCss();
+    return containerEl;
+  }
 
-  return containerEl;
+  private static createButtonContainer() {
+    const buttonContainerEl = document.createElement('div');
+    const buttonContainerCss = css({
+      display: 'flex',
+      gap: 10,
+    });
+    buttonContainerEl.className = buttonContainerCss();
+
+    return buttonContainerEl;
+  }
+
+  private handleReactionClick(reactionType: Reactions) {
+    if (this.config == null) { 
+      throw new Error('invalid config');
+    }
+    // const { owner, repo, issueNumber } = this.config;
+    
+    return new Promise(resolve => resolve(this.config))
+    // return axios.post(`/repos/${owner}/${repo}/issues/${issueNumber}/reactions`, {
+    //   content: reactionType
+    // })
+  }
+
+  public getElement() {
+    return this.containerEl;
+  }
 }
---
+++ packages/library/src/interfaces.ts
@@ -0,0 +1,4 @@
+export const AGREE_REACTION_TYPE = '+1';
+export const DISAGREE_REACTION_TYPE = '-1';
+
+export type Reactions = typeof AGREE_REACTION_TYPE | typeof DISAGREE_REACTION_TYPE;
